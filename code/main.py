4f6650366dabc62f261d63b00a4e96e4bb5c2d21
David Dovo <david.pham.do@gmail.com>
2026-02-11 23:13:58 -0800

    Drawing a basic level and the player
    Iterates through an array of arrays representation of the world map and places a Tile object wherever there is an 'x' and a Player object on the spot labelled 'p'

--- code/main.py
+++ code/main.py
@@ -1,22 +1,32 @@
 import pygame, sys
 from settings import WIDTH, HEIGHT,FPS, TILESIZE, WORLD_MAP
+from level import Level
 
 
 class Game:
     def __init__(self):
+        """Initialize the Game object"""
+
+        # General setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Another DM Zelda Clone')
         self.clock = pygame.time.Clock()
 
+        # Set up a Level
+        self.level = Level()
+
     def run(self):
+        """Running the core Game object event loop"""
         while True:
+            # Basic event-handling logic
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
